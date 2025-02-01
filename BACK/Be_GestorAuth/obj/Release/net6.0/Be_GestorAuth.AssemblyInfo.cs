ff0818544d9c34480b5f7d68f0f842a0ffd7d1b4
Sebastian A. Chico <sebaschico_07@hotmail.com>
2025-01-31 23:15:44 -0500

    publish

--- BACK/Be_GestorAuth/obj/Release/net6.0/Be_GestorAuth.AssemblyInfo.cs
+++ BACK/Be_GestorAuth/obj/Release/net6.0/Be_GestorAuth.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Be_GestorAuth")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3d40bddb5b490780bbffff973c835d1fdc22721")]
 [assembly: System.Reflection.AssemblyProductAttribute("Be_GestorAuth")]
