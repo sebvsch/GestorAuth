f3d40bddb5b490780bbffff973c835d1fdc22721
Sebastian A. Chico <sebaschico_07@hotmail.com>
2025-01-31 23:12:13 -0500

    restore nutget

--- BACK/Be_GestorAuth/obj/Debug/net6.0/Be_GestorAuth.AssemblyInfo.cs
+++ BACK/Be_GestorAuth/obj/Debug/net6.0/Be_GestorAuth.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Be_GestorAuth")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b95363b48150d456bbeeaabd893826802b27b904")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d97f81163f3e709ecedc60a4cb70bb8c7b69ce1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Be_GestorAuth")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Be_GestorAuth")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
